fe800b0b0946596348e1faee17e313e05fccc08e
Neo Lee <neo.lee@gmail.com>
2025-12-18 16:18:35 +0800

    init: xcode new project

---
+++ SCT/SCTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCTApp.swift
+//  SCT
+//
+//  Created by Neo on 2025/12/18.
+//
+
+import SwiftUI
+
+@main
+struct SCTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
